5f54ce84dc9ee4d8a530573393a4544abfd5cebb
FayHagans <elderfayh@gmail.com>
2026-02-13 14:49:17 -0700

    2-13-2026

--- Cafe Sales.sql
+++ Cafe Sales.sql
@@ -34,18 +34,39 @@ END;
 ALTER TABLE cafe_sales_clean
 ALTER COLUMN quantity TYPE int USING quantity::integer;
 
-UPDATE cafe_sales_clean
-SET price_per_unit =
-	CASE WHEN price_per_unit !~ '^[0-9]+$'
-	THEN NULL
-	ELSE price_per_unit
-END;
+SELECT *
+FROM cafe_sales_clean;
+
+UPDATE cafe_sales_clean 
+SET price_per_unit = NULL
+WHERE price_per_unit IN ('UNKNOWN', 'ERROR');
 
 ALTER TABLE cafe_sales_clean
 ALTER COLUMN price_per_unit TYPE decimal(10,2) USING price_per_unit::numeric;
 
 SELECT price_per_unit
 FROM cafe_sales_clean;
+
+
+UPDATE cafe_sales_clean 
+SET total_spent = NULL
+WHERE total_spent IN ('UNKNOWN', 'ERROR');
+
+ALTER TABLE cafe_sales_clean
+ALTER COLUMN total_spent TYPE decimal(10,2) USING total_spent::numeric;
+
+SELECT payment_method
+FROM cafe_sales_clean
+GROUP BY payment_method;
+
+UPDATE cafe_sales_clean 
+SET payment_method = NULL
+WHERE payment_method IN ('UNKNOWN', 'ERROR');
+
+SELECT payment_method
+FROM cafe_sales_clean
+GROUP BY payment_method;
+
 
 
 
